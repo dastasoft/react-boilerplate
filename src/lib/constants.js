aedc4b01b54e01bbea53cba3dd2a022c275e9c3b
dastasoft <dastasoft@protonmail.com>
2021-05-10 23:24:30 +0200

    added react-router-dom

---
+++ src/lib/constants.js
@@ -0,0 +1,5 @@
+export const HOMEPAGE = '/'
+
+export const ROUTES = Object.freeze({
+  HOME: '/',
+})
---
+++ src/routes.js
@@ -0,0 +1,14 @@
+import { Route, Switch } from 'react-router-dom'
+
+import Home from './pages/Home'
+import { ROUTES } from './lib/constants'
+
+export default function Routes() {
+  return (
+    <Switch>
+      <Route path={ROUTES.HOME}>
+        <Home />
+      </Route>
+    </Switch>
+  )
+}
